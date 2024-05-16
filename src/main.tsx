3e269789d0684f9cd099964a82c2d4caeb694193
Henrique Vilanova <henriquevilanova.15@gmail.com>
2024-05-16 15:21:19 -0300

    Estrutura inicial com o Vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
- 
-const App: React.FC = () => {
-    return (
-<div>
-<h1>Hello, world!</h1>
-</div>
-    );
-};
- 
-ReactDOM.render(<App />, document.getElementById('app'));
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
