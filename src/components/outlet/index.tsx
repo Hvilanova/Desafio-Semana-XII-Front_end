8a90d7053bc82ef8c42169892e78a710f2be5080
Henrique Vilanova <henriquevilanova.15@gmail.com>
2024-05-16 17:10:41 -0300

    Commit  contendo header e footer

---
+++ src/components/outlet/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './header'
+import Footer from './footer'
+import {Outlet as Biblioteca} from 'react-router-dom'
+
+const Outlet = () => {
+  return (
+    <> 
+    <Header />
+    <Biblioteca />
+    <Footer />  
+    </>
+  )
+}
+
+export default Outlet
